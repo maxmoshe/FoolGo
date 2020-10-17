9722aa1409ef302fa9c9ea2b6cfddf1b6e59725b
maxmoshe <moshe.bas@gmail.com>
2020-10-17 14:20:52 +0300

    i push first you resolve conflict

--- client-side/my-app/src/components/Register.js
+++ client-side/my-app/src/components/Register.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Login(){
+function Register(){
     return(
         <div id="Login" style={{display: 'flex', flexDirection: "column",alignItems: 'center',border: '1px solid #0e86ca', borderRadius: '5px', padding: 10, backgroundColor: 'grey', width: '20%'}}>
-            <h3 style={{color: '#065294', textDecoration: 'underline'}}>LOGIN IN</h3>
+            <h3 style={{color: '#065294', textDecoration: 'none'}}>Register</h3>
             <input type="text" placeholder="Email address" style={{width: '70%', height: 30, borderRadius: 3}}></input>
             <input type="password" placeholder="Password" autoComplete="false" style={{width: '70%', height: 30, borderRadius: 3, margin: 5}}></input>
-            <button style={{width: '70%', backgroundColor: "#065294", border: 'none', color: 'white', height: 30, borderRadius: 3, margin: 5}}>Log In</button>
-            <a href="google.com" style={{color: "white", textDecoration: 'none', fontSize: 14}}>Register</a>
+            <button style={{width: '70%', backgroundColor: "#065294", border: 'none', color: 'white', height: 30, borderRadius: 3, margin: 5}}>Register</button>
+            <a href="google.com" style={{color: "white", textDecoration: 'none', fontSize: 14}}>Log in</a>
         </div>
     )
 }
 
-export default Login;
+export default Register;
